5e704fa75a04c807bb9dab73aa1f109218eb70c0
rdelos747 <rdelos747@gmail.com>
2019-05-26 20:41:40 -0400

    cleaned up

--- main.cpp
+++ main.cpp
@@ -1,35 +1,80 @@
-#include "../SDL_LIBRARY/FUNCTIONS/FUNCTIONS.h"
-#include "CLASSES/asteroidCreator.h"
-#include "CLASSES/asteroid.h"
-#include "CLASSES/bullet.h"
-#include "CLASSES/ship.h"
+#include <iostream>
+#include "../SDL_LIBRARY/FUNCTIONS/ENGINE.h"
+
+Engine* engine = new Engine();
+
+class Test: public Object {
+public:
+	bool moving = true;
+
+	Test(Engine* parent, int startY)
+	:Object(parent) {
+		//engine.addSprite("../SDL_ASTEROIDS/ASSETS/asteroid_large01.png");
+		y = startY;
+	}
+
+	void update() {
+		if (moving) {
+			x += 1;
+		}
+	}
+
+	void keyDown(int k) {
+		switch(k) {
+			case SDLK_UP: moving = false; break;
+		}
+	}
+
+	void keyUp(int k) {
+		switch(k) {
+			case SDLK_UP: moving = true; break;
+		}
+	}
+};
+
+class TextTest: public Object {
+public:
+	TextTest(Engine* parent, TTF_Font* f)
+	:Object(parent) {
+		x = 10;
+		y = 10;
+		linkFont(f);
+	}
+
+	void update() {
+		setText("heyo", {255,255,255});
+	}
+};
 
 
 void setup() {
-	Ship* ship = new Ship();
-	OBJECTS.push_back(ship);
+	Sprite* testSprite = engine->addSprite("./ASSETS/asteroid_large01.png");
+	TTF_Font* testFont = engine->addFont("./ShareTechMono-Regular.ttf", 10);
 
-	AsteroidCreator* ac = new AsteroidCreator();
-	OBJECTS.push_back(ac);
+	Test* test = new Test(engine, 0);
+	test->linkSprite(testSprite);
+	engine->addObject(test);
+
+	Test* test2 = new Test(engine, 100);
+	test2->linkSprite(testSprite);
+	engine->addObject(test2);
+
+	
+	TextTest* tt = new TextTest(engine, testFont);
+	engine->addObject(tt);
 }
 
 int main(int argc, char* args[]) {
-	if (!SDL_INIT()) {
+	if (!engine->init()) {
 		printf("Failed to init\n");
 	} else {
 		setup();
 		bool runGame = true;
 		while(runGame) {
-			/*
-				For now, all classes will be children of Object,
-				but doing it this way allows for user to add their own
-				items to the update.
-				Maybe thats a bad idea?
-			*/
-			runGame = GAME_UPDATE();
+			runGame = engine->update();
 		}
 	}
 
-	SDL_CLOSE();
+	engine->close();
 	return 0;
 }
